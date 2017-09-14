78d46ec4c84311552b2d4310c2659b072ce518ec
MalachiStoll <mstoll2@babson.edu>
2017-09-14 12:46:02 -0400

    Quadratic Equation Checker. Might need work.

---
+++ function_demo.py
@@ -0,0 +1,36 @@
+def print_lyrics():
+    print("Hey Jude. Don't make it bad.")
+    print("Take a sad song and make it better.")
+
+print_lyrics()  
+
+print(type(print_lyrics))
+
+def repeat_lyrics():
+    print_lyrics()
+    print('Na - na - na - na - na, na - na - na - na')
+    print_lyrics()
+
+repeat_lyrics
+
+def print_twice(name):
+    print(name)
+    print(name)
+
+print_twice('Andrew')
+
+def my_abs(x):
+    print(abs(x))
+
+my_abs(-25)
+
+import math
+
+def quadratic(x):
+    a = 2
+    b = 3
+    c = 4
+    d =  a*(x)**2 + b*(x) + c
+    if d > 0: 
+        print("Nah, U Wrong")
+quadratic(10)
