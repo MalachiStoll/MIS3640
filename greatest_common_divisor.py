3bb6af79daf44fdea4caad396b186c394b29e06b
MalachiStoll <mstoll2@babson.edu>
2017-10-03 13:16:04 -0400

    Last few excersizes and hw's

---
+++ greatest_common_divisor.py
@@ -0,0 +1,10 @@
+def gcf(a,b)
+    if a > b:
+        smaller variable = b
+    else: 
+         smaller variable = b
+    for i in range(smaller variable+1):
+        if ((a % == 0) and (b % == 0)):
+        gcf = i 
+    return gcf
+
---
+++ string_demo.py
@@ -0,0 +1,81 @@
+team = 'New England Patriots'
+
+#letter = team[0]
+#print(len(team))
+
+#print(team[len(letter)-1])
+
+#index = 0
+#for i in range(len(team)):
+ #   if team [i] != '':
+  #  print(team[i])
+
+#for letter in team:
+ #   if letter.isalpha():
+  #      print(letter)
+
+prefixes = 'JKLMNOP'
+suffix = 'ack'
+# for letter in prefixes:
+#     if letter == 'o' or letter == 'q': 
+#         print(letter + 'u' + suffix)
+#     else:
+#         print(letter +suffix)
+
+
+team = 'New England Patriots'
+# print(team[0:11])
+# print(team[12:20])
+
+# print(team[::2])
+
+
+
+
+# team = 'New England Patriots'
+
+# new_team = team[:12] + 'Beavers'
+   
+# print(new_team)
+
+# def find(word, letter):
+#     index = 0
+#     while index < len(word):
+#         if word[index] == letter:
+#             return index
+#         index = index + 1
+#     return -1
+
+# print(find(team, 'W'))
+
+word = 'New England Patriots'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+
+def count(word, letterx):
+    count = 0
+    for letterx in word:
+        if letterx == letterx:
+            count = count +1
+    return count
+
+vowels = 'aeiou'
+number_of_vowels = 0
+for letter in vowels:
+    number_of_vowels += count(team, letter)
+
+print(number_of_vowels)
+
+def count(word, letter):
+    lowerworld = world.lower(
+        c = 0
+        for x in lowerword:
+            if x == letter.lower():
+                c = c+ 1
+        return c 
+    )
+
+
